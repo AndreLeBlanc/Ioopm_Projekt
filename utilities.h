f9c9b70a672d820337dde98374c1e8815a3f7509
Carl Wingardh <c.a.wingardh@gmail.com>
2015-12-11 00:54:59 +0100

    Forgot to add utilities.h

---
+++ utilities.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <stdbool.h>
+
+#ifndef __utilities__
+#define __utilities__
+
+void inputChar(char* message, char* input);
+void inputString(char* message, char* input);
+void inputInt(char* message, int* input);
+void inputFloat(char* message, float* input);
+void inputBool(char* message, bool* input);
+
+#endif
