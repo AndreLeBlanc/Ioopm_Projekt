a071be4248098cc5122a43595996ed04c29587ae
Haubir <jagheterhaubir@gmail.com>
2016-01-10 21:11:03 +0100

    Love life LOL JK

--- stack_traverser.c
+++ stack_traverser.c
@@ -1,4 +1,5 @@
 #include "stack_traverser.h"
+#include <string.h>
 
 #define Dump_registers()			\
   jmp_buf env;					\
@@ -6,13 +7,13 @@
 
 extern void **environ; // bottom of the stack
 
-bool is_pointing_at_heap(void *ptr, heap_t *h/* , size_t bytes */) { 
+bool is_pointing_at_heap(void *ptr, heap_t *h/* , size_t bytes */) {
   long *content = (long *)ptr;
   puts("is_pointing_at_heap är startad.");
   printf("Pointer contains: %ld\n", *(uintptr_t *)ptr);
   printf("Address on stack: %p\n", ptr);
 
-  if (*(uintptr_t *)ptr >= (uintptr_t)get_heap_start(h) && *(uintptr_t *)ptr < (uintptr_t)get_heap_end(h)) {
+  if (*(uintptr_t *)ptr > (uintptr_t)get_heap_start(h) && *(uintptr_t *)ptr < (uintptr_t)get_heap_end(h)) {
     puts("Yes!!!!\n\n\n\n\n\n\n\n\n");
     return true;
   }
@@ -37,14 +38,14 @@ ll_node **get_alive_stack_pointers(heap_t *h) {
 
   if ((uintptr_t)top > (uintptr_t)environ) {
     puts("Stack grows upwards.");
-    while ((uintptr_t *)top > (uintptr_t *)environ) {
+    while ((uintptr_t)top > (uintptr_t)environ) {
       printf("Count %d: \ntop: %p\n", counter, top);
       if (is_pointing_at_heap(top, h)) {
 	if (validate_object(top)) { // checks the pointers metadata to check whether it's pointing at an object or not
 	  void **content = top;
-	  ll_node *added_node = LL_createAndInsertSequentially(root, *content);
+	  ll_node *stackTop = LL_createAndInsertSequentially(root, *content);
 	  //printf("stackTop: %p\n", stackTop);,
-	  printf("Stackpointer %p has valid metadata and was added to the list.\n", LL_getContent(added_node));
+	  printf("Stackpointer %p has valid metadata and was added to the list.\n", LL_getContent(stackTop));
 	  total++;
 	}
       }
@@ -60,9 +61,9 @@ ll_node **get_alive_stack_pointers(heap_t *h) {
       if (is_pointing_at_heap(top, h)) {
 	if (validate_object(top)) { // checks the pointers metadata to check whether it's pointing at an object or not
 	  void **content = top;
-	  ll_node *added_node = LL_createAndInsertSequentially(root, *content);
+	  ll_node *stackTop = LL_createAndInsertSequentially(root, *content);
 	  //printf("stackTop: %p\n", stackTop);,
-	  printf("Stackpointer %p has valid metadata and was added to the list.\n", LL_getContent(added_node));
+	  printf("Stackpointer %p has valid metadata and was added to the list.\n", LL_getContent(stackTop));
 	  total++;
 	}
       }
@@ -76,6 +77,7 @@ ll_node **get_alive_stack_pointers(heap_t *h) {
   //  create_stack_list(top, h, root);
   return root;
 }
+
 
 void print_address(void *listobject){ 
   printf("\n%d\n", listobject);
@@ -93,18 +95,18 @@ void print_stack_list(ll_node **root) {
   int counter = 0;
 
   while (iterator) {
-    printf("\nCounter: %d\n%016\n", counter, (uintptr_t *)LL_getContent(iterator));
+    printf("\nCounter: %d\n%ld\n", counter, *(uintptr_t *)LL_getContent(iterator));
     counter++;
     if (LL_getPrevious(root, iterator)) {
-      printf("%ld has previous pointer at %ld\n", *(uintptr_t *)LL_getContent(iterator), *(uintptr_t *)LL_getContent(LL_getPrevious(root, iterator)));
-      printf("Difference between this pointer %ld and it's previous pointer %ld is %ld bytes\n", *(uintptr_t *)LL_getContent(iterator), *(uintptr_t *)LL_getContent(LL_getPrevious(root, iterator)), (*(uintptr_t *)LL_getContent(iterator) - *(uintptr_t *)LL_getContent(LL_getPrevious(root, iterator))));
+      printf("%p has previous pointer at %p\n", LL_getContent(iterator), LL_getContent(LL_getPrevious(root, iterator)));
+      printf("Difference between this pointer %ld and it's previous pointer %ld is %ld\n", *(uintptr_t *)LL_getContent(iterator), *(uintptr_t *)LL_getContent(LL_getPrevious(root, iterator)), (*(uintptr_t *)LL_getContent(iterator) - *(uintptr_t *)LL_getContent(LL_getPrevious(root, iterator))));
     }
     if (LL_getNext(iterator))
-      printf("%ld has next pointer at %ld\n", *(uintptr_t *)LL_getContent(iterator), *(uintptr_t *)LL_getContent(LL_getNext(iterator)));
+      printf("%p has next pointer at %p\n", LL_getContent(iterator), LL_getContent(LL_getNext(iterator)));
     iterator = LL_getNext(iterator);
   }
 }
-*/ 
+*/
 typedef struct haubir haubir_;
 
 struct haubir {
@@ -112,51 +114,68 @@ struct haubir {
   int amazing;
 };
 
-void print_string(char *string) {
-  printf("String: %s\n", string);
+void set_haubir_string(char *string, haubir_ *to_modify) {
+  strcpy(to_modify->LeBron,string);
 }
 
+void set_haubir_int(int number, haubir_ *to_modify) {
+  to_modify->amazing = number;
+}
 
 int main() {
   // create a new heap
   heap_t *new_heap = h_init(1024, true, 100.0);
   
   // allocate on this heap. For testing purposes
-  // haubir_ *ptr = h_alloc_struct(new_heap, "cccc");
-  // haubir_ *malin = h_alloc_struct(new_heap, "*i");
+  // haubir_ *ptr = h_alloc_struct(new_heap, "cccci");
+  // set_haubir_string("heej", ptr); 
+  // set_haubir_int(45, ptr);
 
-  // char *string = h_alloc_data(new_heap, 1);
-  // strcpy(string, "heej");
-  char *number = h_alloc_data(new_heap, 10);
-  *number = 'c';
+  char *boll = "malin";
+  boll = h_alloc_data(new_heap, 10);
 
+  //haubir_ *malin = h_alloc_struct(new_heap, "*i");
+  char *malin = "malin";
+  malin = h_alloc_data(new_heap, 10);
+  
+  char *string = "heej";
+  string = h_alloc_data(new_heap, 100);
+
+  char *ptr = "ptr";
+  ptr = h_alloc_data(new_heap, 100);
+  printf("Allocated pointer: %ld\n", (uintptr_t)ptr);
+  
   // printf("Address on stack:  %p\n", ptr);
-  
-  // is_pointing_at_heap(ptr, new_heap);
-  
+
+  //  is_pointing_at_heap(ptr, new_heap);
+
   // Dump pointers from the registers to the stack, if any.
   Dump_registers();
   
   printf("hej där\n");
-  
-  // the list contains all alive pointers
-  ll_node **root = get_alive_stack_pointers(new_heap);
 
-  printf("First allocated pointer: %ld\n", (uintptr_t)number);
-  printf("Number: %c\n", *number);
-  // printf("String: %s\n", string);
+  // the list contains all alive pointers
+  ll_node **root = get_alive_stack_pointers(new_heap); 
+
+  printf("haubir_ *malin: %ld\n", (uintptr_t)malin);
+  printf("char *string: %ld\n", (uintptr_t)string);
+  printf("void *ptr: %ld\n", (uintptr_t)ptr);
+  printf("char *string: %ld\n", (uintptr_t)boll);
+
+  // ll_node *bajs = LL_getNodeAtIndex(root, 2); 
+  // char *bajstest = (char *)LL_getContent(bajs); 
+  // if (bajstest) { 
+  //   printf("Bajstest: %s", bajstest); 
+  //   char *outprint = strdup(bajstest);
+  //   printf("Outprint: %s", outprint); 
+  // } 
+  //haubir_ *strukt = LL_getContent(bajs);
+  //haubir_ bajsar = *strukt;
+  //printf("Bajsar: %s\nBajsar: %d\n", bajsar.LeBron, bajsar.amazing);
   // print the list for debugging purposes
   print_stack_list(root);
 
-  // Avreferera roten så får du en pekare till dess nod
-  ll_node *print_node = *root;
 
-  // Avreferera nodepekaren så får du en pekare till dess innehåll (heapaddressen)
-  long *print_node_content = LL_getContent(print_node);
-  
-  char *c = (char *)print_node_content;
-
-  printf("Content of heap pointer %ld is %c\n", print_node_content, *c);
   // deletes the heap we created
   h_delete(new_heap);
   
