a70ddc9c856e4f3ca94f9b215c45d06d8506a627
anle3295 <leblanc_112@hotmail.com>
2016-01-12 00:07:58 +0100

    typ korrekt

--- gc.c
+++ gc.c
@@ -10,4 +10,14 @@
   jmp_buf env;					\
   if (setjmp(env)) {abort();}			\
 
-extern void **environ; //bottom of the stack
+extern char **environ; //bottom of the stack
+
+size_t h_gc(heap_t *h) {
+    if (h.gc_threshold >0.95) {
+      void* bottom = (void*) environ;
+      ll_head stack_p =  get_alive_stack_pointers(h, get_stack_top(), bottom);
+      ll_head heap_p = traverse_pointers_from_LL(stack_p);
+      LL_map(heap_p, h_alloc_compact(h, (void*))); //TODO this isn't correct
+      void post_compact_page_reset(h);
+    }   
+ }
